c5899ecd3cb6607851c217cc186acf60489c3ac8
marian <marian.muller@serli.com>
2024-01-05 10:58:05 +0100

    pullup

--- Core/Src/porting/gb_tgbdual/main_gb_tgbdual.cpp
+++ Core/Src/porting/gb_tgbdual/main_gb_tgbdual.cpp
@@ -618,7 +618,7 @@ void app_main_gb_tgbdual_cpp(uint8_t load_state, uint8_t start_paused, uint8_t s
     GPIO_InitStruct = {0};
     GPIO_InitStruct.Pin = GPIO_PIN_14;
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-    GPIO_InitStruct.Pull = GPIO_PULLDOWN;   // Should be normally HIGH to return 0xff when no counterpart is wired
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
